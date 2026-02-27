925f88a83c141817c5bd4936953ddefc6f8af16d
jai dhuria <jaidhuria002@gmail.com>
2026-02-27 12:54:18 +0530

    SmartBus authentication module initial commit

---
+++ backend/models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: {
+      type: String,
+      required: function () {
+        return !this.googleId;
+      },
+    },
+    googleId: { type: String },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+
+ name:{
+  type:String,
+  required:true
+ },
+
+ email:{
+  type:String,
+  required:true,
+  unique:true
+ },
+
+ password:{
+  type:String,
+  required:true
+ }
+
+});
+
+module.exports = mongoose.model("User",UserSchema);
